a1dfdb4131323528834c32ce043d1560a16e6ad5
DARKZOUL5 <mihailv@inbox.lv>
2026-05-09 23:43:15 +0300

    feat: integrate Yandex Metrika

---
+++ public/js/yandex-metrika.js
@@ -0,0 +1,9 @@
+
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109132511', 'ym');
+
+    ym(109132511, 'init', {ssr:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
